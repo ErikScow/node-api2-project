8545c18b10e1fb53f672f8b3ad350ac21705104c
Erik Scow <erikscow@gmail.com>
2020-05-12 18:06:49 -0700

    Refactored requests concerned with id to return 404 if id doesnt exist. Added delete handler.

--- routes/routes.js
+++ routes/routes.js
@@ -19,21 +19,35 @@ router.post('/', (req, res) => {
 })
 
 router.post('/:id/comments', (req, res) => {
-    if(!req.params.id) {
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    } else if(!req.body.text) {
-        res.status(400).json({ errorMessage: "Please provide text for the comment." })
-    } else {
-        req.body.post_id = req.params.id
-        db.insertComment(req.body)
-            .then(comment => {
-                res.status(201).json(comment)
-            })
-            .catch(error => {
-                console.log('comment post database error: ', error)
-                res.status(500).json({ error: "There was an error while saving the comment to the database" })
-            })
-    }
+
+
+db.findById(req.params.id)
+    .then(post => {
+        if(post.length === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            if(!req.body.text) {
+                res.status(400).json({ errorMessage: "Please provide text for the comment." })
+            } else {
+                req.body.post_id = req.params.id
+                db.insertComment(req.body)
+                    .then(comment => {
+                        res.status(201).json(comment)
+                    })
+                    .catch(error => {
+                        console.log('comment post database error: ', error)
+                        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                    })
+            }
+        } 
+    })
+    .catch(error => {
+        console.log('comment post error could not find post error: ', error)
+        res.status(500).json({ error: "Could not determine whether or not the post exists" })
+    })
+
+
+
 })
 
 router.get('/', (req, res) => {
@@ -48,14 +62,21 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    db.findById(req.params.id)
-        .then(post => {
+
+db.findById(req.params.id)
+    .then(post => {
+        if(post.length === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
             res.status(200).json(post)
-        })
-        .catch(error => {
-            console.log('get by id db error: ', error)
-            res.status(500).json({ error: "The post information could not be retrieved." })
-        })
+        }
+            
+    })
+    .catch(error => {
+        console.log('get by id db error: ', error)
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+
 })
 
 router.get('/:id/comments', (req, res) => {
@@ -70,6 +91,28 @@ router.get('/:id/comments', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
+
+    let postToDelete
+    db.findById(req.params.id)
+        .then(post => {
+            postToDelete = post
+        })
+        .catch(error => {
+            console.log('Unable to retrieve original post: ', error)
+        })
+
+    db.remove(req.params.id)
+        .then(numDeleted => {
+            if(postToDelete.length === 0){
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(postToDelete)
+            }
+        })
+        .catch(error => {
+            console.log('delete db error: ', error)
+            res.status(500).json({ error: "The post could not be removed" })
+        })
 
 })
 
